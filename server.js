6d29345905805955eeea5d77ea6ffb10f483cf1f
Faizan Anwer Ali Rupani <faizananwer2011@gmail.com>
2019-11-21 18:05:44 +0500

    Checking Again

--- server.js
+++ server.js
@@ -1,42 +1,19 @@
-const https = require('https');
-const fs = require('fs');
 const express = require('express');
-const path = require('path');
 const app = express();
 const port = process.env.PORT || 443;
-const CERTIFICATE_DOMAIN = process.env.CERTIFICATE_DOMAIN ||'node-chat.faizanrupani.dev';
 
 //set the template engine ejs
 app.set('view engine', 'ejs');
 
 //middlewares
-var options = {
-    key: fs.readFileSync(path.resolve(__dirname, 'certificates/' + CERTIFICATE_DOMAIN + '/certificate.key')),
-    cert: fs.readFileSync(path.resolve(__dirname, 'certificates/' + CERTIFICATE_DOMAIN + '/certificate.crt')),
-};
-
-app.use(express.static('public', options));
+app.use(express.static('public'));
 
 //routes
 app.get('/', (req, res) => {
     res.render('index')
 });
 
-var DOMAIN = process.env.DOMAIN ||'node-chat-faizan.herokuapp.com';
-//app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-/*var server = https.createServer(options, app);
-server.listen(port, DOMAIN, function (){
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log('App listening at wss://%s:%s and domain: %s', host, port, domain);
-}).on('error', function(err) {
-    if (err.errno === 'EADDRINUSE') {
-        console.log('port busy');
-    } else {
-        console.log(err);
-    }
-});*/
-var server = app.listen(port, DOMAIN, function () {
+var server = app.listen(port, 'node-chat-faizan.herokuapp.com', function () {
     var host = server.address().address;
     var port = server.address().port;
     console.log('App listening at wss://%s:%s', host, port);
