7d7498760907fa7b8a391ae96e13d24d8dfd30c4
Faizan Anwer Ali Rupani <faizananwer2011@gmail.com>
2019-11-21 16:54:32 +0500

    Project Initialization

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const app = express();
+
+//set the template engine ejs
+app.set('view engine', 'ejs');
+
+//middlewares
+app.use(express.static('public'));
+
+//routes
+app.get('/', (req, res) => {
+    res.render('index')
+});
+
+var https = require('https');
+var fs = require('fs');
+
+var domain = proccess.env.DOMAIN ||'node-chat.faizanrupani.dev';
+var options = {
+    key: fs.readFileSync(path.resolve(__dirname, 'certificates/' + domain + '/certificate.key')),
+    cert: fs.readFileSync(path.resolve(__dirname, 'certificates/' + domain + '//certificate.crt')),
+};
+
+var server = https.createServer(options, app);
+var port = proccess.env.PORT || 80;
+server.listen(port, domain, function (){
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('App listening at wss://%s:%s', domain, port);
+}).on('error', function(err) {
+    if (err.errno === 'EADDRINUSE') {
+        console.log('port busy');
+    } else {
+        console.log(err);
+    }
+});
+
+//socket.io instantiation
+const socket_io = require("socket.io");
+const io = socket_io(server);
+
+//listen on every connection
+io.on('connection', (socket) => {
+	console.log('New user connected');
+
+	//default username
+	socket.username = "Anonymous";
+
+    //listen on change_username
+    socket.on('change_username', (data) => {
+        socket.username = data.username;
+    });
+
+    //listen on new_message
+    socket.on('new_message', (data) => {
+        //broadcast the new message
+        io.sockets.emit('new_message', {message : data.message, username : socket.username});
+    });
+
+    //listen on typing
+    socket.on('typing', (data) => {
+    	socket.broadcast.emit('typing', {username : socket.username});
+    });
+});
