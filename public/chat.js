d716c5f4f1c00e43bc4576998ec232c0ba7b925f
Faizan Anwer Ali Rupani <faizananwer2011@gmail.com>
2019-11-21 18:23:00 +0500

    Checking Again

--- public/chat.js
+++ public/chat.js
@@ -1,45 +1,47 @@
 
 $(function(){
    	//make connection
-	//var socket = io.connect('http://localhost:3000');
-	//var socket = io.connect('wss://' + window.location.hostname, {transports: ['websocket'], secure: true});
-	var socket = io.connect('wss://node-chat-faizan.herokuapp.com', {transports: ['websocket'], secure: true});
-	//var socket = io(window.location.hostname, {transports: ['websocket']});
+	var socket;
+	if (location.protocol === 'https:')	{
+		socket = io.connect('wss://' + window.location.hostname + ':' + location.port, {transports: ['websocket'], secure: true});
+	} else {
+		socket = io.connect('ws://' + window.location.hostname + ':' + location.port, {transports: ['websocket'], secure: false});
+	}
 
 	//buttons and inputs
-	var message = $("#message")
-	var username = $("#username")
-	var send_message = $("#send_message")
-	var send_username = $("#send_username")
-	var chatroom = $("#chatroom")
-	var feedback = $("#feedback")
+	var message = $("#message");
+	var username = $("#username");
+	var send_message = $("#send_message");
+	var send_username = $("#send_username");
+	var chatroom = $("#chatroom");
+	var feedback = $("#feedback");
 
 	//Emit message
 	send_message.click(function(){
-		socket.emit('new_message', {message : message.val()})
-	})
+		socket.emit('new_message', {message : message.val()});
+	});
 
 	//Listen on new_message
 	socket.on("new_message", (data) => {
 		feedback.html('');
 		message.val('');
-		chatroom.append("<p class='message'>" + data.username + ": " + data.message + "</p>")
-	})
+		chatroom.append("<p class='message'>" + data.username + ": " + data.message + "</p>");
+	});
 
 	//Emit a username
 	send_username.click(function(){
-		socket.emit('change_username', {username : username.val()})
-	})
+		socket.emit('change_username', {username : username.val()});
+	});
 
 	//Emit typing
 	message.bind("keypress", () => {
-		socket.emit('typing')
-	})
+		socket.emit('typing');
+	});
 
 	//Listen on typing
 	socket.on('typing', (data) => {
-		feedback.html("<p><i>" + data.username + " is typing a message..." + "</i></p>")
-	})
+		feedback.html("<p><i>" + data.username + " is typing a message..." + "</i></p>");
+	});
 });
 
 
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 443;
+const port = process.env.PORT || 3000;
 
 //set the template engine ejs
 app.set('view engine', 'ejs');
