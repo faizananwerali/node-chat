da2b2c7c5d09d8b5849f59767be3b3afa4382c74
Faizan Anwer Ali Rupani <faizananwer2011@gmail.com>
2019-11-21 17:55:52 +0500

    Checking Again

--- server.js
+++ server.js
@@ -3,38 +3,43 @@ const fs = require('fs');
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 443;
+const CERTIFICATE_DOMAIN = process.env.CERTIFICATE_DOMAIN ||'node-chat.faizanrupani.dev';
 
 //set the template engine ejs
 app.set('view engine', 'ejs');
 
 //middlewares
-app.use(express.static('public'));
+var options = {
+    key: fs.readFileSync(path.resolve(__dirname, 'certificates/' + CERTIFICATE_DOMAIN + '/certificate.key')),
+    cert: fs.readFileSync(path.resolve(__dirname, 'certificates/' + CERTIFICATE_DOMAIN + '/certificate.crt')),
+};
+
+app.use(express.static('public', options));
 
 //routes
 app.get('/', (req, res) => {
     res.render('index')
 });
 
-var domain = process.env.DOMAIN ||'node-chat.faizanrupani.dev';
-var options = {
-    key: fs.readFileSync(path.resolve(__dirname, 'certificates/' + domain + '/certificate.key')),
-    cert: fs.readFileSync(path.resolve(__dirname, 'certificates/' + domain + '//certificate.crt')),
-};
-console.log(options);
-
+//var DOMAIN = process.env.DOMAIN ||'node-chat-faizan.herokuapp.com';
 //app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-var server = https.createServer(options, app);
-server.listen(port, domain, function (){
+/*var server = https.createServer(options, app);
+server.listen(port, DOMAIN, function (){
   var host = server.address().address;
   var port = server.address().port;
-  console.log('App listening at wss://%s:%s and host: %s', domain, port, host);
+  console.log('App listening at wss://%s:%s and domain: %s', host, port, domain);
 }).on('error', function(err) {
     if (err.errno === 'EADDRINUSE') {
         console.log('port busy');
     } else {
         console.log(err);
     }
+});*/
+var server = app.listen(port, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('App listening at wss://%s:%s', host, port);
 });
 
 //socket.io instantiation
