72dbd00d9f73e0566dfbef98cf11dfe724a29263
Faizan Anwer Ali Rupani <faizananwer2011@gmail.com>
2019-11-21 17:33:46 +0500

    Checking Again

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
-const https = require('https');
-const fs = require('fs');
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 54494;
+const port = process.env.PORT || 3000;
 
 //set the template engine ejs
 app.set('view engine', 'ejs');
@@ -11,29 +9,14 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 //routes
-app.get('/', (req, res) => {
+app.all('/', (req, res) => {
     res.render('index')
 });
 
-var domain = process.env.DOMAIN ||'node-chat.faizanrupani.dev';
-var options = {
-    key: fs.readFileSync(path.resolve(__dirname, 'certificates/' + domain + '/certificate.key')),
-    cert: fs.readFileSync(path.resolve(__dirname, 'certificates/' + domain + '//certificate.crt')),
-};
-
-//app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-var server = https.createServer(options, app);
-server.listen(port);
-server.listen(port, domain, function (){
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log('App listening at wss://%s:%s', domain, port);
-}).on('error', function(err) {
-    if (err.errno === 'EADDRINUSE') {
-        console.log('port busy');
-    } else {
-        console.log(err);
-    }
+var server = app.listen(port, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('App listening at wss://%s:%s', host, port);
 });
 
 //socket.io instantiation
